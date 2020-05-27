26cae1eb9fc838ac12a715aaa45e5f07a487f73e
root <root@SKCC17N00318.localdomain>
2020-05-27 17:12:53 +0900

    Last Docu

---
+++ k8s.dashboard.aks.sh
@@ -0,0 +1,2 @@
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
+az aks browse --resource-group myResourceGroup --name myAKSCluster
